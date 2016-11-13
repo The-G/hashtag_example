794c69453b18d8e4594c93fc16304c9efec7b4a5
ji hyuk kim <gskjhj@gmail.com>
2016-11-14 04:05:08 +0900

    adjust downcase to Tags_link in helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
   def render_with_hashtags(body)
-    body.gsub(/#\p{Word}+/){|word| link_to word, "/posts/hashtag/#{word.delete('#')}"}.html_safe
+    body.gsub(/#\p{Word}+/){|word| link_to word, "/posts/hashtag/#{word.downcase.delete('#')}"}.html_safe
   end
 end
